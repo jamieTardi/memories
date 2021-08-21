f565c506556f1bd696eb8763c262b17cc3abd0ce
jamie tardi <66372574+jamieTardi@users.noreply.github.com>
2021-08-21 15:37:27 +0100

    adding front end

--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -1,8 +1,66 @@
 import React from 'react';
 import useStyles from './styles';
-const Post = () => {
+import ThumbUpAltIcon from '@material-ui/icons/ThumbUpAlt';
+import DeleteIcon from '@material-ui/icons/Delete';
+import MoreHorizIcon from '@material-ui/icons/MoreHoriz';
+
+import {
+	Card,
+	CardActions,
+	CardContent,
+	CardMedia,
+	Button,
+	Typography,
+} from '@material-ui/core';
+import moment from 'moment';
+
+const Post = ({ post }) => {
 	const classes = useStyles();
-	return <div>POST</div>;
+	console.log(post);
+	return (
+		<div>
+			<Card className={classes.card}>
+				<CardMedia
+					className={classes.media}
+					image={postMessage.selectedFile}
+					title={post.title}
+				/>
+				<div className={classes.overlay}>
+					<Typography variant='h6'>{post.creator}</Typography>
+					<Typography variant='body2'>
+						{moment(post.createdAt).fromNow()}
+					</Typography>
+				</div>
+				<div className={classes.overlay2}>
+					<Button style={{ color: 'white' }} size='small' onClick={() => {}}>
+						<ion-icon name='ellipsis-horizontal-outline'></ion-icon>
+					</Button>
+				</div>
+				<div className={classes.details}>
+					<Typography variant='body2' color='textSecondary'>
+						{post.tags.map((tag) => `#${tag}`)}
+					</Typography>
+				</div>
+				<CardContent>
+					<Typography variant='h5' gutterBottom>
+						{post.message}
+					</Typography>
+				</CardContent>
+				<CardActions className={classes.cardActions}>
+					<Button size='small' color='primary' onClick={() => {}}>
+						<ion-icon name='thumbs-up-outline'></ion-icon>
+						Like
+						{post.likeCount}
+					</Button>
+					<Button size='small' color='primary' onClick={() => {}}>
+						<ion-icon name='trash'></ion-icon>
+						Delete
+						{post.likeCount}
+					</Button>
+				</CardActions>
+			</Card>
+		</div>
+	);
 };
 
 export default Post;
--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -2,17 +2,23 @@ import React from 'react';
 import Post from './Post/Post';
 import useStyles from './styles';
 import { useSelector } from 'react-redux';
+import { Grid, CircularProgress } from '@material-ui/core';
+import { mergeClasses } from '@material-ui/styles';
 
 const Posts = () => {
 	const posts = useSelector((state) => state.posts);
 	const classes = useStyles();
-	console.log(posts);
-	return (
-		<div>
-			<h1>POSTS</h1>
-			<Post />
-			<Post />
-		</div>
+
+	return !posts.length ? (
+		<CircularProgress />
+	) : (
+		<Grid className={classes.container} container alignItems='stretch' spacing>
+			{posts.map((post) => (
+				<Grid key={post._id} item xs={12} sm={6}>
+					<Post post={post} />
+				</Grid>
+			))}
+		</Grid>
 	);
 };
 
